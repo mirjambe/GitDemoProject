295e58c6dcaf53cc722be051b101dc1e1d364cd0
mirjam <mirjam@DESKTOP-3FKMQEO>
2019-10-02 21:31:53 +0200

    first commit

---
+++ src/HelloController.java
@@ -0,0 +1,16 @@
+package io.javabrains.springboot.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String seyHi()
+	{
+		return "Hi!";
+	}
+
+}
+
